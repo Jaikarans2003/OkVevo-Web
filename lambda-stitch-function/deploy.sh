53125287b5baa3f92bf49aa8ff63bd016e674625
jaikaran.pesce <jaikaran.pesce@gmail.com>
2026-04-18 01:00:38 +0530

    Reorganize: Move AI Influencer Lambdas and State Machine into AI-Influencer folder

--- lambda-stitch-function/deploy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# Install only production dependencies
-npm install --production
-
-# Create deployment package (exclude dev dependencies)
-zip -r function.zip . -x "*.git*" "node_modules/aws-sdk/*" "*.zip"
